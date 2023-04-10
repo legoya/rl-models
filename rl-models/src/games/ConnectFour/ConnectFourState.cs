086798a9fb15eaa42a11edfa2e4a01a1858b065d
David <david.peel.ja@gmail.com>
2023-04-10 16:07:03 +1000

    adds connect four state

--- rl-models/src/games/ConnectFour/ConnectFourState.cs
+++ rl-models/src/games/ConnectFour/ConnectFourState.cs
@@ -1,37 +1,41 @@
-// using src.models;
+using src.models;
 
 
-// namespace src.games.ConnectFour;
+namespace src.games.ConnectFour;
 
 
-// public class ConnectFourState : IState
-// {
-//     public List<List<int>> Squares { get; private set;}
-//     public int Size;
+public class ConnectFourState : IState
+{
+    public List<List<int>> Squares { get; private set;}
+    public int NumberOfRows;
+    public int NumberOfColumns;
 
 
-//     public ConnectFourState(int size)
-//     {
-//         Squares = initialiseSquares(size);
-//         Size = size;
+    public ConnectFourState(int numberOfRows, int numberOfColumns)
+    {
+        Squares = initialiseSquares(numberOfRows, numberOfColumns);
+        NumberOfRows = numberOfRows;
+        NumberOfColumns = numberOfColumns;
 
-//     }
+    }
     
-//     public ConnectFourState(ConnectFourState other)
-//     {
+    public ConnectFourState(ConnectFourState other)
+    {
+        Squares = copySquares(other.Squares);
+        NumberOfRows = other.NumberOfRows;
+        NumberOfColumns = other.NumberOfColumns;
+    }
 
-//     }
+    void IState.Update(int player, Move moveLocation)
+    {
+        throw new NotImplementedException();
+    }
 
-//     public void Update(int player, Move moveLocation)
-//     {
+    bool IState.HasWinner(int player, Move moveLocation)
+    {
+        throw new NotImplementedException();
+    }
 
-//     }
-
-//     public bool HasWinner(int player, Move moveLocation)
-//     {
-
-//         return false;
-//     }
 
 //     public override int GetHashCode()
 //     {
@@ -44,33 +48,34 @@
 //         return squaresString.GetHashCode();
 //     }
 
-//     private static List<List<int>> initialiseSquares(int size)
-//     {
-//         var output = new List<List<int>>();
-//         foreach (int i in Enumerable.Range(0, size))
-//         {
-//             output.Add(new List<int>(new int[size]));
-//         }
+    private static List<List<int>> initialiseSquares(int numberOfRows, int numberOfColumns)
+    {
+        var squares = new List<List<int>>();
 
-//         return output;
-//     }
+        foreach (int i in Enumerable.Range(0, numberOfRows))
+        {
+            squares.Add(new List<int>(new int[numberOfColumns]));
+        }
 
-//     private static List<List<int>> copySquares(List<List<int>> otherSquares)
-//     {
-//         var output = new List<List<int>>();
-//         foreach (List<int> row in otherSquares)
-//         {
-//             output.Add(new List<int>(row));
-//         }
+        return squares;
+    }
 
-//         return output;
-//     }
+    private static List<List<int>> copySquares(List<List<int>> otherSquares)
+    {
+        var output = new List<List<int>>();
+        foreach (List<int> row in otherSquares)
+        {
+            output.Add(new List<int>(row));
+        }
 
-//     private void validateMoveLocation(int Row, int Column)
-//     {
-//         if (Squares[Row][Column] != 0)
-//         {
-//             throw new ArgumentException("The supplied move is invalid as it attempts to use an already occupied location");
-//         }
-//     }
-// }
+        return output;
+    }
+
+    //     private void validateMoveLocation(int Row, int Column)
+    //     {
+    //         if (Squares[Row][Column] != 0)
+    //         {
+    //             throw new ArgumentException("The supplied move is invalid as it attempts to use an already occupied location");
+    //         }
+    //     }
+}
